3e7aa84b5340003650bbfb3193c313ca9903a7ed
GustavoMannrich <gustavomannrich98@gmail.com>
2023-02-16 21:52:14 -0300

    Testes de cadastro de usuário e autenticação

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,46 @@
+import { AppError } from '../../../../errors/AppError';
+import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory';
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe('Create Category', () => {
+    beforeAll(() => {
+        categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+        createCategoryUseCase = new CreateCategoryUseCase(
+            categoriesRepositoryInMemory
+        );
+    });
+
+    it('should be able to create a new category', async () => {
+        const category = {
+            name: 'Category Test',
+            description: 'Category Description Test',
+        };
+
+        await createCategoryUseCase.execute(category);
+
+        const categoryCreated = await categoriesRepositoryInMemory.findByName(
+            category.name
+        );
+
+        expect(categoryCreated).toHaveProperty('id');
+    });
+
+    it('should not be able to create categories with duplicate names', async () => {
+        expect(async () => {
+            const category = {
+                name: 'Category Test',
+                description: 'Category Description Test',
+            };
+
+            await createCategoryUseCase.execute(category);
+
+            const categoryCreated =
+                await categoriesRepositoryInMemory.findByName(category.name);
+
+            expect(categoryCreated).toHaveProperty('id');
+        }).rejects.toBeInstanceOf(AppError);
+    });
+});
