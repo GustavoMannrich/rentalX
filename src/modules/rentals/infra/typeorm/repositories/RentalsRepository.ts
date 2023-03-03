ce5708d3f3192f9b837c07cafe1a35d3a57c6263
GustavoMannrich <gustavomannrich98@gmail.com>
2023-03-02 22:10:40 -0300

    devolução do veículo alugado

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -1,34 +1,52 @@
-import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
-import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
-import { getRepository, Repository } from "typeorm";
-import { Rental } from "../entities/Rental";
+import { ICreateRentalDTO } from '@modules/rentals/dtos/ICreateRentalDTO';
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { getRepository, Repository } from 'typeorm';
+import { Rental } from '../entities/Rental';
 
 class RentalsRepository implements IRentalsRepository {
-  private repository: Repository<Rental>;
+    private repository: Repository<Rental>;
 
-  constructor() {
-    this.repository = getRepository(Rental);
-  }
+    constructor() {
+        this.repository = getRepository(Rental);
+    }
 
-  async create({ car_id, user_id, expected_return_date }: ICreateRentalDTO): Promise<Rental> {
-    const rental = this.repository.create({
-      car_id,
-      user_id,
-      expected_return_date,
-    });
+    async create({
+        id,
+        car_id,
+        user_id,
+        expected_return_date,
+        end_date,
+        total,
+    }: ICreateRentalDTO): Promise<Rental> {
+        const rental = this.repository.create({
+            id,
+            car_id,
+            user_id,
+            expected_return_date,
+            end_date,
+            total,
+        });
 
-    await this.repository.save(rental);
+        await this.repository.save(rental);
 
-    return rental;
-  }
+        return rental;
+    }
 
-  async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    return await this.repository.findOne({ car_id });
-  }
+    async findOpenRentalByCar(car_id: string): Promise<Rental> {
+        return await this.repository.findOne({
+            where: { car_id, end_date: null },
+        });
+    }
 
-  async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    return await this.repository.findOne({ user_id });
-  }
+    async findOpenRentalByUser(user_id: string): Promise<Rental> {
+        return await this.repository.findOne({
+            where: { user_id, end_date: null },
+        });
+    }
+
+    async findById(id: string): Promise<Rental> {
+        return await this.repository.findOne(id);
+    }
 }
 
 export { RentalsRepository };
--- src/modules/rentals/repositories/inMemory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/inMemory/RentalsRepositoryInMemory.ts
@@ -1,32 +1,44 @@
-import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
-import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
-import { IRentalsRepository } from "../IRentalsRepository";
+import { ICreateRentalDTO } from '@modules/rentals/dtos/ICreateRentalDTO';
+import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
+import { IRentalsRepository } from '../IRentalsRepository';
 
 class RentalsRepositoryInMemory implements IRentalsRepository {
-  rentals: Rental[] = [];
+    rentals: Rental[] = [];
 
-  async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    return this.rentals.find((rental) => rental.car_id === car_id && !rental.end_date);
-  }
+    async findOpenRentalByCar(car_id: string): Promise<Rental> {
+        return this.rentals.find(
+            (rental) => rental.car_id === car_id && !rental.end_date
+        );
+    }
 
-  async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    return this.rentals.find((rental) => rental.user_id === user_id && !rental.end_date);
-  }
+    async findOpenRentalByUser(user_id: string): Promise<Rental> {
+        return this.rentals.find(
+            (rental) => rental.user_id === user_id && !rental.end_date
+        );
+    }
 
-  async create({ car_id, user_id, expected_return_date }: ICreateRentalDTO): Promise<Rental> {
-    const rental = new Rental();
+    async create({
+        car_id,
+        user_id,
+        expected_return_date,
+    }: ICreateRentalDTO): Promise<Rental> {
+        const rental = new Rental();
 
-    Object.assign(rental, {
-      car_id,
-      user_id,
-      expected_return_date,
-      start_date: new Date(),
-    });
+        Object.assign(rental, {
+            car_id,
+            user_id,
+            expected_return_date,
+            start_date: new Date(),
+        });
 
-    this.rentals.push(rental);
+        this.rentals.push(rental);
 
-    return rental;
-  }
+        return rental;
+    }
+
+    findById(id: string): Promise<Rental> {
+        throw new Error('Method not implemented.');
+    }
 }
 
 export { RentalsRepositoryInMemory };
