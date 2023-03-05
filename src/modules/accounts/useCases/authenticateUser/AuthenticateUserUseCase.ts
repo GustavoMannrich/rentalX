408a728102f7fd0f3a719d2912d9dae53c42a539
GustavoMannrich <gustavomannrich98@gmail.com>
2023-03-05 01:17:54 -0300

    Refresh token

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,3 +1,6 @@
+import auth from '@config/auth';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
 import { AppError } from '@shared/infra/http/errors/AppError';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
@@ -15,16 +18,28 @@ interface IResponse {
         email: string;
     };
     token: string;
+    refresh_token: string;
 }
 
 @injectable()
 class AuthenticateUserUseCase {
     constructor(
-        @inject('UsersRepository') private usersReposistory: IUsersRepository
+        @inject('UsersRepository') private usersReposistory: IUsersRepository,
+        @inject('UsersTokensRepository')
+        private usersTokensRepositorys: IUsersTokensRepository,
+        @inject('DayjsDateProvider')
+        private dateProvider: IDateProvider
     ) {}
 
     async execute({ email, password }: IRequest): Promise<IResponse> {
         const user = await this.usersReposistory.findByEmail(email);
+        const {
+            expires_in_token,
+            secret_token,
+            secret_refresh_token,
+            expires_in_refresh_token,
+            expires_refresh_token_days,
+        } = auth;
 
         if (!user) {
             throw new AppError('Email or password incorrect!');
@@ -36,17 +51,33 @@ class AuthenticateUserUseCase {
             throw new AppError('Email or password incorrect!');
         }
 
-        const token = sign({}, '240fee48cfdd0ec9ac6b6876bdef1d4f', {
+        const token = sign({}, secret_token, {
             subject: user.id,
-            expiresIn: '1d',
+            expiresIn: expires_in_token,
+        });
+
+        const refresh_token = sign({ email }, secret_refresh_token, {
+            subject: user.id,
+            expiresIn: expires_in_refresh_token,
+        });
+
+        const refresh_token_expires_date = this.dateProvider.addDays(
+            expires_refresh_token_days
+        );
+
+        await this.usersTokensRepositorys.create({
+            user_id: user.id,
+            expires_date: refresh_token_expires_date,
+            refresh_token,
         });
 
         const tokenReturn: IResponse = {
+            token,
             user: {
                 name: user.name,
                 email: user.email,
             },
-            token,
+            refresh_token,
         };
 
         return tokenReturn;
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import { AppError } from '../errors/AppError';
 import { UsersRepository } from '../../../../modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
+import auth from '@config/auth';
 
 interface IPayload {
     sub: string;
@@ -13,6 +15,7 @@ export async function ensureAuthenticated(
     next: NextFunction
 ) {
     const authHeader = request.headers.authorization;
+    const usersTokenRepository = new UsersTokensRepository();
 
     if (!authHeader) {
         throw new AppError('Token missing!', 401);
@@ -24,11 +27,13 @@ export async function ensureAuthenticated(
     try {
         const { sub: user_id } = verify(
             token,
-            '240fee48cfdd0ec9ac6b6876bdef1d4f'
+            auth.secret_refresh_token
         ) as IPayload;
 
-        const usersRepository = new UsersRepository();
-        const user = await usersRepository.findById(user_id);
+        const user = await usersTokenRepository.findByUserIdAndRefreshToken(
+            user_id,
+            token
+        );
 
         if (!user) {
             throw new AppError('User does not exists!', 401);
