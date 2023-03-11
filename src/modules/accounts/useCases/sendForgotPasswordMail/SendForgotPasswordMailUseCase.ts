2de248db268a3e37b29c6553bbf4cbfbde817b49
GustavoMannrich <gustavomannrich98@gmail.com>
2023-03-11 17:11:04 -0300

    Envio de email usando o AWS SES

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,62 +1,50 @@
-import { IUsersRepository } from '@modules/accounts/repositories/IUserRepository';
-import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
-import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
-import { IMailProvider } from '@shared/container/providers/MailProvider/IMailProvider';
-import { AppError } from '@shared/infra/http/errors/AppError';
-import { inject, injectable } from 'tsyringe';
-import { v4 as uuidv4 } from 'uuid';
-import { resolve } from 'path';
+import { IUsersRepository } from "@modules/accounts/repositories/IUserRepository";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
+import { AppError } from "@shared/infra/http/errors/AppError";
+import { inject, injectable } from "tsyringe";
+import { v4 as uuidv4 } from "uuid";
+import { resolve } from "path";
 
 @injectable()
 class SendForgotPasswordMailUseCase {
-    constructor(
-        @inject('UsersRepository') private usersRepository: IUsersRepository,
-        @inject('UsersTokensRepository')
-        private usersTokensRepository: IUsersTokensRepository,
-        @inject('DayjsDateProvider')
-        private dateProvider: IDateProvider,
-        @inject('EtherealMailProvider')
-        private mailProvider: IMailProvider
-    ) {}
+  constructor(
+    @inject("UsersRepository") private usersRepository: IUsersRepository,
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider,
+    @inject("MailProvider")
+    private mailProvider: IMailProvider
+  ) {}
 
-    async execute(email: string) {
-        const user = await this.usersRepository.findByEmail(email);
+  async execute(email: string) {
+    const user = await this.usersRepository.findByEmail(email);
 
-        const templatePath = resolve(
-            __dirname,
-            '..',
-            '..',
-            'views',
-            'emails',
-            'forgotPassword.hbs'
-        );
+    const templatePath = resolve(__dirname, "..", "..", "views", "emails", "forgotPassword.hbs");
 
-        if (!user) {
-            throw new AppError('User does not exists!');
-        }
-
-        const token = uuidv4();
-
-        const expires_date = this.dateProvider.addHours(3);
-
-        await this.usersTokensRepository.create({
-            refresh_token: token,
-            user_id: user.id,
-            expires_date,
-        });
-
-        const variables = {
-            name: user.name,
-            link: `${process.env.FORGOT_MAIL_URL}${token}`,
-        };
-
-        await this.mailProvider.sendMail(
-            email,
-            'Recuperação de senha',
-            variables,
-            templatePath
-        );
+    if (!user) {
+      throw new AppError("User does not exists!");
     }
+
+    const token = uuidv4();
+
+    const expires_date = this.dateProvider.addHours(3);
+
+    await this.usersTokensRepository.create({
+      refresh_token: token,
+      user_id: user.id,
+      expires_date,
+    });
+
+    const variables = {
+      name: user.name,
+      link: `${process.env.FORGOT_MAIL_URL}${token}`,
+    };
+
+    await this.mailProvider.sendMail(email, "Recuperação de senha", variables, templatePath);
+  }
 }
 
 export { SendForgotPasswordMailUseCase };
