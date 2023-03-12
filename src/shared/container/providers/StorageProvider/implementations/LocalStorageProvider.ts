80abdd3789d60fb993d9ce8d0fae98cee719db9c
GustavoMannrich <gustavomannrich98@gmail.com>
2023-03-12 00:09:06 -0300

    correção no import do utils/file

--- src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
@@ -1,24 +1,21 @@
-import { IStorageProvider } from '../IStorageProvider';
-import fs from 'fs';
-import { resolve } from 'path';
-import upload from '@config/upload';
-import { deleteFile } from 'utils/file';
+import { IStorageProvider } from "../IStorageProvider";
+import fs from "fs";
+import { resolve } from "path";
+import upload from "@config/upload";
+import { deleteFile } from "@utils/file";
 
 class LocalStorageProvider implements IStorageProvider {
-    async save(file: string, folder: string): Promise<string> {
-        fs.promises.rename(
-            resolve(upload.tmpFolder, file),
-            resolve(`${upload.tmpFolder}/${folder}`, file)
-        );
+  async save(file: string, folder: string): Promise<string> {
+    fs.promises.rename(resolve(upload.tmpFolder, file), resolve(`${upload.tmpFolder}/${folder}`, file));
 
-        return file;
-    }
+    return file;
+  }
 
-    async delete(file: string, folder: string): Promise<void> {
-        const filename = resolve(`${upload.tmpFolder}/${folder}`, file);
+  async delete(file: string, folder: string): Promise<void> {
+    const filename = resolve(`${upload.tmpFolder}/${folder}`, file);
 
-        deleteFile(filename);
-    }
+    deleteFile(filename);
+  }
 }
 
 export { LocalStorageProvider };
