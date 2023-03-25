81200f7b43b85ffa5e4e1249c64c757eac223f73
GustavoMannrich <gustavomannrich98@gmail.com>
2023-03-25 11:42:35 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -10,6 +10,7 @@ import '../typeorm';
 import '../../container';
 import { AppError } from './errors/AppError';
 import upload from '@config/upload';
+import cors from 'cors';
 
 createConnection();
 const app = express();
@@ -21,6 +22,7 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
